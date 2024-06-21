bce1c0cb9b9032e9fe7b1dca6909e852c9b7adde
JozMroz <praca.jozefmroZ@GMAIL.COM>
2024-06-21 20:11:12 +0200

    Update planar_quadrotor.cpp

--- PlanarQuadrotor/planar_quadrotor.cpp
+++ PlanarQuadrotor/planar_quadrotor.cpp
@@ -1,21 +1,75 @@
+#include "planar_quadrotor.h"
 #include <iostream>
 #include <random>
-
-#include "planar_quadrotor.h"
 
 PlanarQuadrotor::PlanarQuadrotor() {
     std::random_device r;
     std::default_random_engine generator(r());
     std::normal_distribution<float> distribution(0.0, 1.0);
-    auto gaussian = [&] (int) {return distribution(generator);};
+    auto gaussian = [&](int) { return distribution(generator); };
 
     z = Eigen::VectorXf::NullaryExpr(6, gaussian);
+
+    // Inicjalizacja SDL2 dla audio
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        std::cerr << "SDL nie mogło się zainicjalizować! SDL_Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
+
+    // Załaduj plik dźwiękowy
+    if (SDL_LoadWAV("D:\\syf\\mixkit-drone-logo-presentation-2751.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+        std::cerr << "Nie udalo sie zaladować pliku dzwiekowego! SDL_Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
+
+    // Otwórz urządzenie audio
+    device_id = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+    if (device_id == 0) {
+        std::cerr << "Nie udalo sie otworzyc urzadzenia audio! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_FreeWAV(wav_buffer);
+        SDL_Quit();
+        std::exit(1);
+    }
 }
 
-PlanarQuadrotor::PlanarQuadrotor(Eigen::VectorXf z): z(z) {}
+PlanarQuadrotor::PlanarQuadrotor(Eigen::VectorXf z) : z(z) {
+    // Inicjalizacja SDL2 dla audio
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        std::cerr << "SDL nie moglo sie zainicjalizowac! SDL_Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
+
+    // Załaduj plik dźwiękowy
+    if (SDL_LoadWAV("D:\\syf\\mixkit-drone-logo-presentation-2751.wav", &wav_spec, &wav_buffer, &wav_length) == NULL) {
+        std::cerr << "Nie udalo sie zaladować pliku dzwiekowego! SDL_Error: " << SDL_GetError() << std::endl;
+        std::exit(1);
+    }
+
+    // Otwórz urządzenie audio
+    device_id = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+    if (device_id == 0) {
+        std::cerr << "Nie udalo sie otworzyc urzadzenia audio! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_FreeWAV(wav_buffer);
+        SDL_Quit();
+        std::exit(1);
+    }
+}
+
+PlanarQuadrotor::~PlanarQuadrotor() {
+    // Zwolnij plik dźwiękowy
+    SDL_CloseAudioDevice(device_id);
+    SDL_FreeWAV(wav_buffer);
+
+    // Zamknij SDL
+    SDL_Quit();
+}
 
 void PlanarQuadrotor::SetGoal(Eigen::VectorXf z_goal) {
     this->z_goal = z_goal;
+}
+
+void PlanarQuadrotor::SetInput(Eigen::Vector2f input) {
+    this->input = input;
 }
 
 Eigen::VectorXf PlanarQuadrotor::GetState() {
@@ -91,32 +145,53 @@ void PlanarQuadrotor::DoCalcTimeDerivatives() {
     float u_2 = input[1];
 
     z_dot.block(0, 0, 3, 1) = z.block(3, 0, 3, 1);
-    
+
     /* See http://underactuated.mit.edu/acrobot.html#section3 3.3.1 */
     float x_dotdot = -(u_1 + u_2) * sin(theta) / m;
     float y_dotdot = (u_1 + u_2) * cos(theta) / m - g;
     float theta_dotdot = r * (u_1 - u_2) / I;
 
-    z_dot.block(3, 0, 3, 1) << x_dotdot, y_dotdot, theta_dotdot;
+    z_dot[3] = x_dotdot;
+    z_dot[4] = y_dotdot;
+    z_dot[5] = theta_dotdot;
 }
 
 void PlanarQuadrotor::DoUpdateState(float dt) {
-    /* Euler integration */
-    z += dt * z_dot;
+    z += z_dot * dt;
 }
 
-void PlanarQuadrotor::SetInput(Eigen::Vector2f input) {
-    this->input = input;
-}
-
-Eigen::VectorXf PlanarQuadrotor::Update(Eigen::Vector2f &input, float dt) {
+Eigen::VectorXf PlanarQuadrotor::Update(Eigen::Vector2f& input, float dt) {
     SetInput(input);
     DoCalcTimeDerivatives();
     DoUpdateState(dt);
+
+    // Sprawdź, czy dron się porusza i odpowiednio odtwórz dźwięk
+    if (z_dot.norm() > 0.01f) {
+        SDL_ClearQueuedAudio(device_id); // Wyczyść poprzednią kolejkę audio
+        SDL_QueueAudio(device_id, wav_buffer, wav_length); // Dodaj dźwięk do kolejki
+        SDL_PauseAudioDevice(device_id, 0); // Wznów odtwarzanie audio
+    }
+    else {
+        SDL_PauseAudioDevice(device_id, 1); // Wstrzymaj odtwarzanie audio
+    }
 
     return z;
 }
 
 Eigen::VectorXf PlanarQuadrotor::Update(float dt) {
-    return Update(input, dt);
+    DoCalcTimeDerivatives();
+    DoUpdateState(dt);
+
+    // Sprawdź, czy dron się porusza i odpowiednio odtwórz dźwięk
+    if (z_dot.norm() > 0.01f) {
+        SDL_ClearQueuedAudio(device_id); // Wyczyść poprzednią kolejkę audio
+        SDL_QueueAudio(device_id, wav_buffer, wav_length); // Dodaj dźwięk do kolejki
+        SDL_PauseAudioDevice(device_id, 0); // Wznów odtwarzanie audio
+    }
+    else {
+        SDL_PauseAudioDevice(device_id, 1); // Wstrzymaj odtwarzanie audio
+    }
+
+    return z;
 }
+
